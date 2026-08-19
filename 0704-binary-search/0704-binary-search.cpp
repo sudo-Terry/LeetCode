1d8499deed0dcff9d068570f31978ce081d95231
Kihoon Kim <contact.terrykim@gmail.com>
2026-08-19 18:30:38 +0900

    Time: 0 ms (100%), Space: 31.2 MB (99.11%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2; // 오버플로우 방지
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+};
